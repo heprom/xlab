d862ef3631663c9e61633738dd3edbdcf5bd3b77
maxxime <maxime.pelerin@outlook.com>
2019-03-21 15:12:07 +0100

    Update xlab.py
    Testé et fonctionnel sur le serveur excepté la visualisation puisque la version de matplotlib installée (datée...) ne fonctionne pas dans un thread. Cela devrait être fonctionnel sur le nouveau serveur.

--- xlab.py
+++ xlab.py
@@ -1,15 +1,29 @@
-from __future__ import print_funtion, division
+from __future__ import print_function, division
 #%% librairie standard
 import os
 import time
 from threading import Thread
+import traceback, sys
 #%% dépendances tierces
 import PyTango
 import numpy as np
 from matplotlib import pyplot as plt
 import tifffile
 #%%
-
+def monotonous(mt, speed=1e-3, direction=-1):
+    """mt is a MechanicalTest instance
+    speed in mm/s (if good settings of the device !)
+    direction is a relative non-zero float"""
+    if mt.actuator_isset and mt.actuator_ischecked:
+        direction = direction / abs(direction)
+        mt.actuator.velocity = speed
+        if direction > 0:
+            mt.actuator.forward()  # attention arrêt, limites...
+        else:
+            mt.actuator.backward()  # attention, gérer l'arret moteur
+    else:
+        raise ValueError("You have to check the actuator")
+#%%
 
 class MechanicalTest(object):
 
@@ -17,8 +31,8 @@ class MechanicalTest(object):
     _sai = {'sai': 'd13-1-mines/ca/sai.1'}
     _camera = {'basler': 'd13-1-mines/dt/baslermines.1'}
 
-    def __init__(self, config='xlab'):
-        self.load_config(config)
+    def __init__(self, config='bulky_xlab', channel=-1):
+        self.load_config(config, channel)
         self.signal_sensors = []
         self.signal_sensors_lbl = []
         self.image_sensors = []
@@ -30,8 +44,8 @@ class MechanicalTest(object):
         self.actuator_velocity = self.actuator.velocity
         self.actuator_isset = True
         self.actuator_ischecked = False
-        self.signal_senors.append(lambda: self.actuator.position)
-        self.signal_sensors_lbl .append('position [mm]')
+        self.signal_sensors.append(lambda: self.actuator.position)
+        self.signal_sensors_lbl.append('position [mm]')
 
     def check_actuator(self):
         if self.actuator.state() is PyTango._PyTango.DevState.STANDBY:
@@ -49,8 +63,13 @@ class MechanicalTest(object):
             self.actuator_ischecked = False
         return self.actuator_ischecked
 
-    def load_config(self, config):
-        pass
+    def load_config(self, config, channel):
+        if config == 'xlab_bulky':
+            self.set_actuator('d13-1-mines/ex/bulky')
+            self.add_sai_sensor('d13-1-mines/ca/sai.1', channel=channel)
+        elif config == 'psiche_bulky':  # non fonctionnel
+            self.set_actuator('tango-mines:d13-1-mines/ex/bulky')
+            self.add_sai_sensor('i03-c-c00/ca/sai.1', channel=channel)
 
     def save_config(self, filename):
         pass
@@ -59,7 +78,7 @@ class MechanicalTest(object):
         self.signal_sensors.append(function)
         self.signal_sensors_lbl.append(label)
 
-    def add_sai_sensor(self, name, attribute='', channel=-1, label=None):
+    def add_sai_sensor(self, name, attribute='', channel=-1, label=None, gain=1):
         if not hasattr(self, 'sai'):
             if '/' not in name:
                 name = self._sai[name]
@@ -69,7 +88,7 @@ class MechanicalTest(object):
         if hasattr(self.sai, attribute):
             if label is None:
                 label = name + '.' + attribute + ' [V]'
-            self.add_signal_sensor(lambda _: getattr(self.sai, attribute), label)
+            self.add_signal_sensor(lambda: getattr(self.sai, attribute), label)
         else:
             raise TypeError
         ## start the sai !
@@ -89,19 +108,19 @@ class MechanicalTest(object):
             filename += '_{:05d}'
         self.image_sensors_filename.append(filename + ext)
 
-    def add_camera(self, name):
+    def add_camera(self, name, save_pattern):
         """for tango camera devices"""
         if '/' not in name:
             name = self._camera[name]
         self.camera = PyTango.DeviceProxy(name)
-        ## limitation une camera !
-        ## tester l'état, arreter un mode live...
+        # limitation une camera !
+        # tester l'état, arreter un mode live...
         def snap():
             self.camera.Snap()
             while self.camera.state() is PyTango._PyTango.DevState.RUNNING:
                 time.sleep(0.001)
             return self.camera.image
-        self.add_image_sensor(snap)
+        self.add_image_sensor(snap, save_pattern)
 
     def set_sample(self, sample_name, base_directory):
         self.sample_name = sample_name
@@ -134,117 +153,116 @@ class MechanicalTest(object):
             print('All seems correct')
             return True
 
-    class Data_Acquisition(Thread):  # modulaire mais moins efficace que la version initiale
-
-        def __init__(self, mt, dt=0.12):
-            self.dt = dt
-            self.mt = mt
-            Thread.__init__(self)
-
-        def _wait(self, t_ref):
-            try:
-                time.sleep(t_ref + self.dt - time.time())
-            except ValueError:
-                print('Warning: Impossible to maintain the requested rate!')
-
-        def run(self):
-            counter = 0
-            with open(self.mt.out_directory + self.mt.sample_name + '_data.log', 'a') as data_file:
-                pattern = ';'.join(['{:.4f}' for _ in ['time'] + self.mt.signal_sensors]) + '\n'
-                t0 = time.time()
-                while self.mt.data_acquisition:
-                    t_ref = time.time()
-                    data_file.write(pattern.format(time.time() - t0, *[f() for f in self.mt.signal_sensors]))
-                    for i, get_img in enumerate(self.mt.image_sensors):
-                        path = self.mt.out_directory + self.mt.sample_name + self.mt.image_sensors_filename[i].format(i, counter)
-                        tifffile.imsave(path, get_img)  # (basler.image/2**4).astype(np.uint8))
-                    self._wait(t_ref)
-                    counter += 1
-
-    class Data_Display(Thread):
-
-        def __init__(self, mt, ind=[0, 1], gain=1, dt=0.12, figsize=(20, 10)):
-            self.dt = dt
-            self.mt = mt
-            self.x = ind[0]
-            self.y = ind[-1]
-            self.lx = [0]
-            self.ly = [0]
-            self.gain = gain
-            self.figsize = figsize
-            Thread.__init__(self)
-
-        def _wait(self, t_ref):
-            try:
-                time.sleep(t_ref + self.dt - time.time())
-            except ValueError:
-                print('Warning: Impossible to maintain the requested rate!')
-
-        def run(self):
-            x0 = self.mt.signal_sensors[self.x]()
-            y0 = self.mt.signal_sensors[self.y]()
-            for i in range(10):
-                self.l_x.append(self.mt.signal_sensors[self.x]() - x0)
-                self.l_y.append((self.mt.signal_sensors[self.x]() - y0) * self.gain)
-                time.sleep(.05)
-            fig = plt.figure(self.figsize)
-            plt.ion()
-            plt.show()
-            ax = fig.add_subplot(111)
-            ax.set_xlabel("Position moteur (mm)")
-            ax.set_ylabel("Force (N)")
-            line, = ax.plot(self.lx, self.ly)
-            while self.mt.data_display:
-                self.l_x.append(self.mt.signal_sensors[self.x]() - x0)
-                self.l_y.append((self.mt.signal_sensors[self.x]() - y0) * self.gain)
-                ax.set_xlim(min(self.l_x), max(self.l_x))
-                ax.set_ylim(min(self.l_y), max(self.l_y))
-                line.set_data(self.l_m, self.l_c)
-
-    def set_load_path(self, function=monotonous, args=(1e-3, -1), kwargs={}):
+    def set_load_path(self, function=monotonous, args=tuple(), **kwargs):
         self.load_path = function
         self.load_path_args = args
         self.load_path_kwargs = kwargs
 
-    def run(self, **kwargs):
+    def run(self, display=True, acquisition=True, **kwargs):
         # acquisition
-        self.data_acquisition = True
-        kw = {k: v for k, v in kwargs.items() if k in ['dt']}
-        self.data_acquisition_thread = self.Data_Acquisition(self, **kw)
+        if acquisition:
+            self.data_acquisition = True
+            kw = {k: v for k, v in kwargs.items() if k in ['dt']}
+            self.data_acquisition_thread = DataAcquisition(self, **kw)
         # visualisation
-        self.data_display = True
-        self.data_display_thread = self.Data_Display(self, dt=.5)
+        if display:
+            self.data_display = True
+            self.data_display_thread = DataDisplay(self, dt=.5)
         # mouvement
         try:
-            self.data_acquisition_thread.run()
-            self.data_display_thread.run()
-            self.load_path(*self.load_path_args, **self.load_path_kwargs)
+            self.data_acquisition_thread.start() if acquisition else None
+            self.data_display_thread.start() if display else None
+            self.load_path(self, *self.load_path_args, **self.load_path_kwargs)
         except BaseException as e:
-            self.actuator.stop()
-            self.data_acquisition = False
             self.last_error = e
+            t, msg, tb = sys.exc_info()
+            import traceback
+            traceback.print_tb(tb)
+            self.stop()
+            
+    def stop(self):
+        self.actuator.stop()
+        self.data_acquisition = False
+        self.data_display = False
 
+
+class DataAcquisition(Thread):  # modulaire mais moins efficace que la version initiale
+
+    def __init__(self, mt, dt=0.12):
+        self.dt = dt
+        self.mt = mt
+        Thread.__init__(self)
+
+    def _wait(self, t_ref):
+        try:
+            time.sleep(t_ref + self.dt - time.time())
+        except ValueError:
+            print('Warning: Impossible to maintain the requested rate!')
+
+    def run(self):
+        counter = 0
+        with open(os.path.join(self.mt.out_directory, self.mt.sample_name + '_data.log'), 'a') as data_file:
+            pattern = ';'.join(['{:.4f}' for _ in ['time'] + self.mt.signal_sensors]) + '\n'
+            t0 = time.time()
+            while self.mt.data_acquisition:
+                t_ref = time.time()
+                data_file.write(pattern.format(time.time() - t0, *[f() for f in self.mt.signal_sensors]))
+                for i, get_img in enumerate(self.mt.image_sensors):
+                    path = self.mt.out_directory + self.mt.sample_name + self.mt.image_sensors_filename[i].format(i, counter)
+                    tifffile.imsave(path, get_img)  # (basler.image/2**4).astype(np.uint8))
+                self._wait(t_ref)
+                counter += 1
+
+class DataDisplay(Thread):
+
+    def __init__(self, mt, ind=[0, 1], gain=1, dt=0.12, figsize=(20, 10)):
+        self.dt = dt
+        self.mt = mt
+        self.x = ind[0]
+        self.y = ind[-1]
+        self.l_x = []
+        self.l_y = []
+        self.gain = gain
+        self.figsize = figsize
+        Thread.__init__(self)
+
+    def _wait(self, t_ref):
+        try:
+            time.sleep(t_ref + self.dt - time.time())
+        except ValueError:
+            print('Warning: Impossible to maintain the requested rate!')
+
+    def run(self):
+        x0 = self.mt.signal_sensors[self.x]()
+        y0 = self.mt.signal_sensors[self.y]()
+        for i in range(10):
+            self.l_x.append(self.mt.signal_sensors[self.x]() - x0)
+            self.l_y.append((self.mt.signal_sensors[self.y]() - y0) * self.gain)
+            time.sleep(.05)
+        self.fig = plt.figure()
+        plt.ion()
+        plt.show()
+        ax = self.fig.add_subplot(111)
+        ax.set_xlabel("Position moteur (mm)")
+        ax.set_ylabel("Force (N)")
+        line, = ax.plot(self.l_x, self.l_y)
+        while self.mt.data_display:
+            self.l_x.append(self.mt.signal_sensors[self.x]() - x0)
+            self.l_y.append((self.mt.signal_sensors[self.y]() - y0) * self.gain)
+            ax.set_xlim(min(self.l_x), max(self.l_x))
+            ax.set_ylim(min(self.l_y), max(self.l_y))
+            line.set_data(self.l_x, self.l_y)
+        plt.savefig(os.path.join(self.mt.out_directory, self.mt.sample_name + '_plot.pdf'))
 
 #%%
-def monotonous(mt, speed=1e-3, direction=-1):
-    """mt is a MechanicalTest instance
-    speed in mm/s (if good settings of the device !)
-    direction is a relative non-zero float"""
-    if mt.actuator_isset and mt.actuator_ischecked:
-        direction = direction / abs(direction)
-        mt.actuator.velocity = speed
-        if direction > 0:
-            mt.actuator.forward()  # attention arrêt, limites...
-        else:
-            mt.actuator.backwards()  # attention, gérer l'arret moteur
-    else:
-        raise ValueError
 
-#%%
+import traceback
+
 if __name__ == '__main__':
     test = MechanicalTest()
     test.set_actuator('bulky')
-    test.add_sai_sensor('sai', channel=1)
-    test.set_sample('test_xlab.py', '/root/Desktop/Maxime')
-    if test.check_all():
-        test.run()
+    test.add_sai_sensor('sai', channel=0)
+    test.set_sample('test_1', '/root/Desktop/Maxime')
+    test.set_load_path(speed=3e-3)
+    if test.check_all(strict=False):
+        test.run(display=False)
